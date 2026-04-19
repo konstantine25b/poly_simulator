a4c77f79e16b8df30c46d44fc5c2e120f7f36f6b
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2026-04-19 12:51:45 +0400

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import polymarketIcon from "../assets/polymarket.jpg";
 
 export default function App() {
   return (
     <div className="shell">
-      <img src={polymarketIcon} alt="" className="mark" width={72} height={72} />
     </div>
   );
 }
